ae57d3fd768d5b976d94eed542113663ba24c82c
Tanner Barcelos <barcelos.m.tanner@gmail.com>
2022-12-13 19:39:36 -0800

    Testing TW and auto format

--- app/page.tsx
+++ app/page.tsx
@@ -1,3 +1,5 @@
 export default function Home() {
-  return <h1 className='text-3xl font-bold underline '>Hello world!</h1>;
+  return (
+    <h1 className='text-3xl font-bold text-red-500 underline'>Hello world!</h1>
+  );
 }
