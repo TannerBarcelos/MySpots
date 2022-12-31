8029141bbf3317ed6d8e51188418c9eb40e5fd2b
Tanner Barcelos <barcelos.m.tanner@gmail.com>
2022-12-30 18:32:07 -0800

    Renaming

--- app/head.tsx
+++ app/head.tsx
@@ -1,7 +1,7 @@
 export default function Head() {
   return (
     <>
-      <title>MySpots</title>
+      <title>Spottr</title>
       <meta content='width=device-width, initial-scale=1' name='viewport' />
     </>
   );
